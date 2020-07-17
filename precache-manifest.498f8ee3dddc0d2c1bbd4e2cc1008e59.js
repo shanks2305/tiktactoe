c7295b36c18fe4d0a1a46cb583c0875d88f88102
shanks2305 <contactshishirbahuguna@gmail.com>
2020-07-17 20:26:01 +0530

    Updates

---
+++ precache-manifest.498f8ee3dddc0d2c1bbd4e2cc1008e59.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "153b8d838d4b4e5b49ea4b3abb2e2e19",
+    "url": "/tiktactoe/index.html"
+  },
+  {
+    "revision": "20f97fa1a92f279040e3",
+    "url": "/tiktactoe/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "7d6c7acdb8699c909cde",
+    "url": "/tiktactoe/static/css/main.1a4cc275.chunk.css"
+  },
+  {
+    "revision": "20f97fa1a92f279040e3",
+    "url": "/tiktactoe/static/js/2.eb4f3064.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/tiktactoe/static/js/2.eb4f3064.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d6c7acdb8699c909cde",
+    "url": "/tiktactoe/static/js/main.7fdb6bdd.chunk.js"
+  },
+  {
+    "revision": "a12063e210a4eab753cf",
+    "url": "/tiktactoe/static/js/runtime-main.1a2d9dfd.js"
+  }
+]);
